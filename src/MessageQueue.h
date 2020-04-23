ad937bd568fbde9dcfa86e1cfea0e4fe5c6950e6
Anastasia Golovin <asgolovin@protonmail.com>
2020-04-23 20:28:32 +0200

    Move definitions from MessageQueue.cpp to header

--- src/MessageQueue.h
+++ src/MessageQueue.h
@@ -1,19 +1,29 @@
 #ifndef MESSAGEQUEUE_H
 #define MESSAGEQUEUE_H
 
+#include <condition_variable>
 #include <deque>
 #include <mutex>
-#include <condition_variable>
 
-template <class T>
-class MessageQueue
-{
+template <class T> class MessageQueue {
 public:
-  void send(T &&msg);
-  T receive();
+  void send(T &&msg) {
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.push_back(std::move(msg));
+    _cond.notify_one();
+  }
+
+  T receive() {
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _cond.wait(uLock, [this] { return !_queue.empty(); });
+    // use FIFO order
+    T msg = std::move(_queue.front());
+    _queue.pop_front();
+    return msg;
+  }
 
 private:
-  std::deque<double[2]> _queue;
+  std::deque<T> _queue;
   std::condition_variable _cond;
   std::mutex _mutex;
 };
